1cc60c8d3522305d9f5b6c23e116e1d26d3c01d3
WillPrata <wilson.prata@soulasalle.com.br>
2025-03-20 20:04:21 -0300

    EX 2.C

---
+++ EX 2.C
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+            i++;
+        }
+
+        scanner.close();
+    }
+}
